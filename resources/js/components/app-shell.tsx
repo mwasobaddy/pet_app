662b78c52defe71078c5ae2e6b38c2411adb6a2b
Hiikey7 <kelvinramsiel@gmail.com>
2026-04-21 05:30:13 +0300

    Set sidebar to collapsed by default on initial load

--- resources/js/components/app-shell.tsx
+++ resources/js/components/app-shell.tsx
@@ -17,5 +17,5 @@ export function AppShell({ children, variant = 'sidebar' }: Props) {
         );
     }
 
-    return <SidebarProvider defaultOpen={false}>{children}</SidebarProvider>;
+    return <SidebarProvider defaultOpen={isOpen}>{children}</SidebarProvider>;
 }
