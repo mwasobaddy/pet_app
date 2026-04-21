f63cf7434dca1b3aac3955e94e6450a32245aa95
Hiikey7 <kelvinramsiel@gmail.com>
2026-04-21 03:01:05 +0300

    UI: Update sidebar components and app sidebar header layout

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -13,7 +13,7 @@ export function AppSidebarHeader({
 
     return (
         <header
-            className={`flex md:hidden fixed z-[11] shadow-lg w-screen bg-white dark:bg-gray-900 h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4${isHiddenRoute ? ' hidden' : ''}`}
+            className={`hidden fixed z-[11] shadow-lg w-screen bg-white dark:bg-gray-900 h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4${isHiddenRoute ? ' hidden' : ''}`}
         >
             <div className="flex items-center gap-2">
                 <SidebarTrigger className="-ml-1" />
--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -302,7 +302,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "bg-background relative flex max-w-full min-h-svh flex-1 flex-col",
+        "bg-background relative flex max-w-full min-h-svh flex-1 flex-col !pb-0 !m-0 !rounded-none",
         "peer-data-[variant=inset]:min-h-[calc(100svh-(--spacing(4)))] md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-0",
         className
       )}
